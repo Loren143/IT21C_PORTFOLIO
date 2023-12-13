a800809c3e2b7366f68f7d0e0cec16a1af5fdebc
Loren143 <dacolloren143>
2023-12-13 13:12:48 +0800

    added my Sort Class

--- src/Array/Sort.java
+++ src/Array/Sort.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Array;
 
-/**
- *
- * @author user
- */
+
 public class Sort {
     
+
+    
+    public static void main(String[] args) {
+        int[] numbers = {12, 9, 4, 11, 7, 5};
+        Array a = new Array(numbers);
+        a.setNumbers(numbers);
+    }
+}
+
+class Array {
+    int[] numbers;
+
+    public Array(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] newNumbers) {
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                   
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
 }
