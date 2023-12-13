520673c9249da361f5ac9c2f8b43a7c447d31ff8
Loren143 <dacolloren143>
2023-12-13 14:32:23 +0800

    added my Student class

---
+++ src/basic_oop/student.java
@@ -0,0 +1,45 @@
+
+package basic_oop;
+
+public class student {
+    
+
+    String name, section, motto;
+
+    String name() {
+
+        return name;
+
+    }
+
+    String getSection() {
+
+        return section;
+
+    }
+
+    String getMotto() {
+
+        return motto;
+
+    }
+
+    void setName(String newName) {
+
+        name = newName;
+
+    }
+
+    void setSection(String newSection) {
+
+        section = newSection;
+
+    }
+
+    void setMotto(String newMotto) {
+
+        motto = newMotto;
+
+    }
+
+}
