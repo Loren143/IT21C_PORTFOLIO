09efc1b7addbbf4d16553dae53342ba2a4aa5d95
Loren143 <dacolloren143>
2023-12-13 14:18:38 +0800

    added my polymorphism_food class

---
+++ src/Polymorphism/polymorphism_food.java
@@ -0,0 +1,12 @@
+
+package Polymorphism;
+
+
+public class polymorphism_food extends polymorphism_book {
+    
+    
+   
+     public void MyHabbit(){
+        System.out.println("Nag kaon si");  
+     }
+}
