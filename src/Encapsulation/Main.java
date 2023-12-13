ff03bccec2af145040a10d39d57036f24834c87b
Loren143 <dacolloren143>
2023-12-13 13:54:18 +0800

    added my Main encapsulation class

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,14 @@
+
+package Encapsulation;
+
+public class Main {
+    
+    
+    public static void main(String[]args){
+        
+            Areaa rectangle = new Areaa(9,3);
+            rectangle.getArea();
+        }
+    
+    
+}
