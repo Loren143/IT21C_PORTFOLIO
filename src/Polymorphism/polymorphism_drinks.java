27e36c11397af0ba275ac908f1703cc728896eaf
Loren143 <dacolloren143>
2023-12-13 14:15:47 +0800

    added my polymorphism_drinks class

---
+++ src/Polymorphism/polymorphism_drinks.java
@@ -0,0 +1,14 @@
+
+package Polymorphism;
+
+
+public class polymorphism_drinks extends polymorphism_book {
+    
+
+   
+    
+        public void MyHabbit(){
+        System.out.println("Nag inom si" );  
+     
+        }
+}
